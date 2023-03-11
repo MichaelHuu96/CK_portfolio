03e4bc2f1e45b8696f8494b07e051781eb426b04
MichaelHuu <michaelhuu1809@gmail.com>
2023-03-11 05:44:31 +0100

    updated pages and added cases page

--- Javascript/index.js
+++ Javascript/index.js
@@ -13,21 +13,24 @@ document.onmousemove = e => {
   container.style.backgroundPositionX = -e.clientX * 0.04 + "px";
   container.style.backgroundPositionY = -e.clientY * 0.05 + "px";
 
-  cubeformation.style.transform = "translateX(" + (-e.clientX * 0.025) + "px) translateY(" + (-e.clientY * 0.02) + "px)";
-  michael.style.transform = "translateX(" + (-e.clientX * 0.025) + "px) translateY(" + (-e.clientY * 0.02) + "px)";
+  cubeformation.style.transform = "translateX(" + (-e.clientX * 0.02) + "px) translateY(" + (-e.clientY * 0.02) + "px)";
+  michael.style.transform = "translateX(" + (-e.clientX * 0.02) + "px) translateY(" + (-e.clientY * 0.02) + "px)";
 
 
   for (var i = 0; i < cursor_perspectives.length; i++) {
-    cursor_perspectives[i].style.transform = "translateX(" + (-e.clientX * 0.002) + "px) translateY(" + (-e.clientY * 0.02) + "px)";
+    cursor_perspectives[i].style.transform = "translateX(" + (-e.clientX * 0.006) + "px) translateY(" + (-e.clientY * 0.006) + "px)";
   }
 
   for (var i = 0; i < cursor_perspectives_high.length; i++) {
-    cursor_perspectives_high[i].style.transform = "translateX(" + (-e.clientX * 0.008) + "px) translateY(" + (-e.clientY * 0.02) + "px)";
+    cursor_perspectives_high[i].style.transform = "translateX(" + (-e.clientX * 0.008) + "px) translateY(" + (-e.clientY * 0.008) + "px)";
   }  
   for (var i = 0; i < cursor_perspectives_bg.length; i++) {
     cursor_perspectives_bg[i].style.transform = "translateX(" + (-e.clientX * 0.025) + "px) translateY(" + (-e.clientY * 0.02) + "px)";
   }
-  
+  /* figures moves with background instead of animation
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.transform = "translateX(" + (-e.clientX * 0.025) + "px) translateY(" + (-e.clientY * 0.02) + "px)";
+  }*/
 }
 
 
@@ -60,5 +63,137 @@ elements.forEach((element) => {
 
   element.addEventListener('touchend', () => {
     element.classList.remove('move');
+  });
+});
+
+/*   Move elements around so they stick
+
+elements.forEach((element) => {
+  const easingFactor = 0.1;
+  let rafId = null;
+  let offsetX = 0;
+  let offsetY = 0;
+
+  function animate() {
+    offsetX *= 1 - easingFactor;
+    offsetY *= 1 - easingFactor;
+    element.style.setProperty('--offset-x', `${offsetX}px`);
+    element.style.setProperty('--offset-y', `${offsetY}px`);
+    if (Math.abs(offsetX) > 0.1 || Math.abs(offsetY) > 0.1) {
+      rafId = requestAnimationFrame(animate);
+    } else {
+      element.classList.remove('move');
+    }
+  }
+
+  element.addEventListener('mousemove', (e) => {
+    const { left, top, width, height } = element.getBoundingClientRect();
+    const x = e.clientX - left;
+    const y = e.clientY - top;
+    offsetX = Math.max(-width, Math.min(width, (width / 2 - x) * 1));
+    offsetY = Math.max(-height, Math.min(height, (height / 2 - y) * 1));
+    element.classList.add('move');
+    if (!rafId) {
+      rafId = requestAnimationFrame(animate);
+    }
+  });
+
+  element.addEventListener('mouseleave', () => {
+    if (rafId) {
+      cancelAnimationFrame(rafId);
+      rafId = null;
+    }
+    offsetX = 0;
+    offsetY = 0;
+    element.classList.remove('move');
+  });
+
+  element.addEventListener('touchmove', (e) => {
+    const { left, top, width, height } = element.getBoundingClientRect();
+    const x = e.touches[0].clientX - left;
+    const y = e.touches[0].clientY - top;
+    offsetX = Math.max(-width, Math.min(width, (width / 2 - x) * 1));
+    offsetY = Math.max(-height, Math.min(height, (height / 2 - y) * 1));
+    element.classList.add('move');
+    if (!rafId) {
+      rafId = requestAnimationFrame(animate);
+    }
+  });
+
+  element.addEventListener('touchend', () => {
+    if (rafId) {
+      cancelAnimationFrame(rafId);
+      rafId = null;
+    }
+    offsetX = 0;
+    offsetY = 0;
+    element.classList.remove('move');
+  });
+});
+
+
+
+/*   Move elements around so they stick
+
+*/
+
+elements.forEach((element) => {
+  const easingFactor = 0.1;
+  let rafId = null;
+  let offsetX = 0;
+  let offsetY = 0;
+
+  function animate() {
+    offsetX *= 1 - easingFactor;
+    offsetY *= 1 - easingFactor;
+    element.style.setProperty('--offset-x', `${offsetX}px`);
+    element.style.setProperty('--offset-y', `${offsetY}px`);
+    if (Math.abs(offsetX) > 0.1 || Math.abs(offsetY) > 0.1) {
+      rafId = requestAnimationFrame(animate);
+    } else {
+      element.classList.remove('move');
+    }
+  }
+
+  element.addEventListener('mousemove', (e) => {
+    const { left, top, width, height } = element.getBoundingClientRect();
+    const x = e.clientX - left;
+    const y = e.clientY - top;
+    offsetX = Math.max(-width, Math.min(width, (width / 2 - x) * 1));
+    offsetY = Math.max(-height, Math.min(height, (height / 2 - y) * 1));
+    element.classList.add('move');
+    if (!rafId) {
+      rafId = requestAnimationFrame(animate);
+    }
+  });
+
+  element.addEventListener('mouseleave', () => {
+    if (rafId) {
+      cancelAnimationFrame(rafId);
+      rafId = null;
+    }
+    offsetX = 0;
+    offsetY = 0;
+  });
+
+  element.addEventListener('touchmove', (e) => {
+    const { left, top, width, height } = element.getBoundingClientRect();
+    const x = e.touches[0].clientX - left;
+    const y = e.touches[0].clientY - top;
+    offsetX = Math.max(-width, Math.min(width, (width / 2 - x) * 1));
+    offsetY = Math.max(-height, Math.min(height, (height / 2 - y) * 1));
+    element.classList.add('move');
+    if (!rafId) {
+      rafId = requestAnimationFrame(animate);
+    }
+  });
+
+  element.addEventListener('touchend', () => {
+    if (rafId) {
+      cancelAnimationFrame(rafId);
+      rafId = null;
+    }
+    offsetX = 0;
+    offsetY = 0;
   });
 });
--- Javascript/myScript.js
+++ Javascript/myScript.js
@@ -1,27 +1,39 @@
 $(document).ready(function() {
-    if(!$('#myCanvas').tagcanvas({
-      
-      zoomMax: 1, // set max zoom to 1
-      zoomMin: 1, // set min zoom to 1
-      outlineColour: '#fff',
-      outlineThickness: 0,
-      reverse: true,
-      depth: 1,
-      maxSpeed: 0.03,
-      textFont: null,
-      textColour: null,
-      weightMode:'both',
-      weight: true,
-      
-      
-      
-      
+  if(!$('#myCanvas').tagcanvas({
+    shape: "vcylinder", // set shape to vcylinder
+    zoomMax: 1, // set max zoom to 1
+    zoomMin: 1, // set min zoom to 1
+ 
+    reverse: true,
+    outlineThickness:0,
+    bgOutlineThickness:0,
+    depth: 1,
+    maxSpeed: 0.03,
+    textFont: null,
+    textColour: null,
+    weightMode:'both',
+    weight: true,
+    bgOutlineThickness: 0,
+    imageRadius:20,
+    initial:[-0.05,0],
+    radiusX:0.8,
+    radiusZ:1,
+    radiusY:0.6,
+    depth:1,
+    outlineOffset: 0,
+    shadowOffset:"[0,2]",
+    lock:"y",
+    
+    
+  }, "tags")) {
+    $("#myCanvasContainer").hide();
+  }
 
-    },'tags')) {
-      // something went wrong, hide the canvas container
-      $('#myCanvasContainer').hide();
-    }
+  $("#myCanvasContainer").scroll(function() {
+    var scrollLeft = $(this).scrollLeft();
+    $("#tags").css("left", -scrollLeft);
   });
+});
 
 
   function cloudhide(){
